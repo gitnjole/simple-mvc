bb53ef1e6ef515e70c849bdfd6f57f4e382b9115
Antonio Cahun <cahunantonio1@gmail.com>
2024-01-26 15:23:56 +0100

    feat(migrations): Added migrations files

---
+++ migrations/migration01_initial.php
@@ -0,0 +1,14 @@
+<?php
+
+class Migration01_initial
+{
+    public function Lift()
+    {
+        echo "Appying migration";
+    }
+
+    public function Dip()
+    {
+        echo "Down migration";
+    }
+}
