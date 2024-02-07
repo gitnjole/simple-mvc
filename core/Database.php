623f7bc0397e3e8439ebf1d36a0228c014a89c97
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-07 23:31:28 +0100

    refactor(Database): switch to MariaDB from PostgreSQL

--- core/Database.php
+++ core/Database.php
@@ -9,8 +9,8 @@ class Database
     public function __construct(array $config)
     {
         $dsn = $config['DSN'] ?? '';
-        $user = $config['USER'] ?? '';
-        $password = $config['PASSWORD'] ?? '';
+        $user = $config['DB_USER'] ?? '';
+        $password = $config['DB_PASSWORD'] ?? '';
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
