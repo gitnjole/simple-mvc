a1c2a8e5a822c08ebbc4ed05316ddc8a5d566586
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-28 18:15:39 +0100

    feat: implement dynamic log in/log out links

--- web/index.php
+++ web/index.php
@@ -35,6 +35,7 @@ $app->router->get('/login', [AuthController::class, 'login']);
 $app->router->post('/login', [AuthController::class, 'login']);
 $app->router->get('/register', [AuthController::class, 'register']);
 $app->router->post('/register', [AuthController::class, 'register']);
+$app->router->get('/logout', [AuthController::class, 'logout']);
 /********************************************************************* */
 
 $app->run();
