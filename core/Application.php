160c9ff3dd5069571201d4c566e4a0a95ecde3da
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-28 18:18:12 +0100

    feat: implement method for checking if a user is set

--- core/Application.php
+++ core/Application.php
@@ -67,4 +67,9 @@ class Application
         $this->user = null;
         $this->session->remove('user');
     }
+
+    public static function isGuest()
+    {
+        return !self::$app->user;
+    }
 }
